510802feb6485df7f591c0a84364f9f3d9c1a476
ibrahim wais <67577024+Ibrahim-wais-22@users.noreply.github.com>
2022-10-05 23:54:35 -0700

    add

--- components/AnotherData1.jsx
+++ components/AnotherData1.jsx
@@ -1,9 +1,10 @@
 function AnotherData1(){
+    
     return(
         <div className="anotherData1">
             {/* THis is anatherData1 */}
         <div className="name1">
-
+    
         <h5>new</h5>
         <h5>new</h5>
         <h5>new</h5>
