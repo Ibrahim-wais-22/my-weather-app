1f3a643fbd3e2ec31f632f689d2b38c0c994250f
ibrahim wais <67577024+Ibrahim-wais-22@users.noreply.github.com>
2022-10-05 23:55:55 -0700

    create weather page.js

---
+++ pages/weather.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import PageWeather from "../components/PageWeather";
+
+const Weather = () => {
+    const [cities, setCities] = useState();
+    const [selectedCity, setSelectedCity] = useState("Tokyo");
+    const [weather, setWeather] = useState();
+  
+    const handleClick = (name) => {
+      setSelectedCity(name);
+    };
+
+    useEffect(() => {
+        fetch("http://countriesnow.space/api/v0.1/countries/capital/")
+          .then((response) => response.json())
+          .then((response) => {
+            setCities(response);
+          })
+          .catch((err) => console.error(err));
+        console.log(cities);
+      }, []);
+    
+
+      
+      useEffect(() => {
+        fetch(
+          `http://api.weatherstack.com/current?access_key=5c4d64c114b0322c2b3cb0ec4a031979&query=${selectedCity}`
+        )
+          .then((response) => response.json())
+          .then((response) => {
+            console.log(response);
+            setWeather(response);
+          })
+          .catch((err) => console.error(err));
+        console.log(weather);
+      }, [selectedCity]);
+
+  
+      if (!cities || !weather) {
+        return <div>Loading...</div>;
+      }
+      console.log(weather);
+      return (
+        <div className="cointainer">
+          {" "}
+          <PageWeather cities={cities} weather={weather} handleClick={handleClick} />
+          {/* <PageWeather  /> */}
+        </div>
+  );
+};
+
+export default Weather;
